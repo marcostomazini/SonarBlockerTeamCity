3b3da2b95a2dc1ec72e8c41f353697de2d042459
unknown <marcos.tomazini@gmail.com>
2015-07-29 08:25:45 -0300

    remocao do id sem uso

--- SonarBlockerTeamCity/SonarBlockerTeamCity/Program.cs
+++ SonarBlockerTeamCity/SonarBlockerTeamCity/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Text;
+using System.IO;
 
 namespace SonarBlockerTeamCity
 {
@@ -12,8 +13,8 @@ namespace SonarBlockerTeamCity
     {
         private static Base _base = new Base();
         private static string url = string.Empty;
+        private static string path = string.Empty;        
         private static string urlSonar = ("{0}/api/rules/show?key=");
-        private static string id = string.Empty;
 
         static void Main(string[] args)
         {
@@ -35,19 +36,60 @@ namespace SonarBlockerTeamCity
             }
 
             url = args[0];
-            id = args[1];
-            urlSonar = string.Format(urlSonar, args[2]);
+            path = args[0];
+            urlSonar = string.Format(urlSonar, args[1]);
 
             _base.Hello();
 
-            var urlFull = string.Format(url, id);
-
+            System.Console.WriteLine("Sonar: " + urlSonar);
+            System.Console.WriteLine("Url: " + url);
 
             var sonarMetricas = new List<Sonar>();
-            using (WebClient client = new WebClient())
+            //using (WebClient client = new WebClient())
+            //{
+            //    var sourceCodePage = client.DownloadData(urlFull);
+            //    string pageHtmlReturn = Encoding.UTF8.GetString(sourceCodePage);
+
+            //    var indexPerResource = pageHtmlReturn.IndexOf("issuesPerResource =");
+            //    var jsonCutted = pageHtmlReturn.Substring(indexPerResource);
+
+            //    var index = jsonCutted.IndexOf("issuesPerResource =");
+            //    var indexEnd = jsonCutted.IndexOf("];");
+            //    var totalToCut = (indexEnd + 3) - index;
+
+            //    var jsonFiltered = jsonCutted.Substring(index, totalToCut);
+
+
+            //    var removeCharInvalids = jsonFiltered // poderia ser usado um expressao regular
+            //        .Replace("\n", string.Empty)
+            //        .Replace("\r", string.Empty)
+            //        .Replace("  ", string.Empty)
+            //        .Replace("[],", string.Empty)
+            //        .Replace("issuesPerResource =", string.Empty)
+            //        .Replace(";", string.Empty)
+            //        .Replace("[]", string.Empty);
+
+            //    var listaFull = JsonConvert.DeserializeObject<List<List<Sonar>>>(removeCharInvalids);
+
+            //    foreach (var item in listaFull)
+            //    {
+            //        foreach (var sitem in item)
+            //        {
+            //            sonarMetricas.Add(sitem);
+            //        }
+            //    }
+            //}
+
+            //if (!File.Exists(path))
+            //{
+            //    Console.BackgroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Arquivo não existe");
+            //    Environment.Exit(0);
+            //}
+
+            using (StreamReader sr = new StreamReader(path))
             {
-                var sourceCodePage = client.DownloadData(urlFull);
-                string pageHtmlReturn = Encoding.UTF8.GetString(sourceCodePage);
+                string pageHtmlReturn = sr.ReadToEnd();
 
                 var indexPerResource = pageHtmlReturn.IndexOf("issuesPerResource =");
                 var jsonCutted = pageHtmlReturn.Substring(indexPerResource);
@@ -84,23 +126,34 @@ namespace SonarBlockerTeamCity
             var critical = sonarMetricas.Where(x => x.s == "critical"); 
             var blocker = sonarMetricas.Where(x => x.s == "blocker");
 
-            foreach (var item in blocker)
+            try
             {
-                System.Console.WriteLine("Critical: " + item.r);                
-
-                using (WebClient client = new WebClient())
+                foreach (var item in blocker)
                 {
+                    System.Console.WriteLine("Critical: " + item.r);
 
-                    var sourceCodePage = client.DownloadData(urlSonar + item.r.Remove(0, 1));
-                    string pageHtmlReturn = Encoding.UTF8.GetString(sourceCodePage);
+                    using (WebClient client = new WebClient())
+                    {
 
-                    var sonarRules = JsonConvert.DeserializeObject<SonarRules>(pageHtmlReturn);
+                        var sourceCodePage = client.DownloadData(urlSonar + item.r.Remove(0, 1));
+                        string pageHtmlReturn = Encoding.UTF8.GetString(sourceCodePage);
 
-                    throw new Exception("Sonar >>> Qualidade Critica em ::: " + sonarRules.rule.name);
+                        var sonarRules = JsonConvert.DeserializeObject<SonarRules>(pageHtmlReturn);
+
+                        throw new Exception("Sonar >>> " + sonarRules.rule.name);
+                    }                    
+                    throw new Exception("Sonar >>> " + item.r.Remove(0, 1));
                 }
-
-                throw new Exception("Sonar >>> Qualidade Critica em " + item.r.Remove(0, 1));
             }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+
+                Environment.Exit(1);
+            }
+
+            Console.Read();
         }
     }
 }
